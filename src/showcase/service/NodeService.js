ee24dec1b6d7d830f3201f95b807afff68ae218d
Arif Hasan <arif25169@gmail.com>
2018-08-13 17:09:16 +0600

    For Github

--- src/showcase/service/NodeService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export class NodeService {
-    
-    getNodes() {
-        return axios.get('showcase/resources/demo/data/nodes.json')
-                .then(res => res.data.root);
-    }
-}
